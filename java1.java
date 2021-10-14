31294cf25410ec0e3cac6e69156f6ff7acf5b396
Shaima Abdul Kader <32644823+shaima123@users.noreply.github.com>
2021-10-14 17:04:12 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,16 @@
+
+
+public class java1 {
+
+	public static void main(String[] args) {  
+	
+			int a=1;  
+			int b=2;  
+			a+=4;
+			b-=4;
+			System.out.println(a);  
+			System.out.println(b);  
+			 
+	}
+}
+
---
+++ logical.java
@@ -0,0 +1,14 @@
+
+public class logical {
+
+	public static void main(String[] args) {
+		int a=10;  
+		int b=5;  
+		int c=20;  
+		System.out.println(a>b&&a<c);
+		System.out.println(c>b&&a>c);
+		System.out.println(a<b||a<c);
+		System.out.println(b>a||b>c);
+	}
+
+}
