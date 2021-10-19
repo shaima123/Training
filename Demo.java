78f1597366f0da6a94ef48aec8081e6e048b2cc1
Shaima Abdul Kader <32644823+shaima123@users.noreply.github.com>
2021-10-19 17:31:41 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,48 @@
+
+interface polygon
+{
+    void input();
+    void area();
+}
+class Circle implements polygon
+{
+    int r = 0;
+    double pi = 3.14, ar = 0;
+    @Override
+    public void input()
+    {
+        r = 5;
+    }
+    @Override
+    public void area()
+    {
+        ar = pi * r * r;
+        System.out.println("Area of circle:"+ar);
+    }
+}
+class Rectangle extends Circle
+{
+    int l = 0, b = 0;
+    double ar;
+    public void input()
+    {
+        super.input();
+        l = 6;
+        b = 4;
+    }
+    public void area()
+    {
+        super.area();
+        ar = l * b;
+        System.out.println("Area of rectangle:"+ar);
+    }
+}
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        Rectangle obj = new Rectangle();
+        obj.input();
+        obj.area();
+    }
+}
